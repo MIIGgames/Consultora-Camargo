849addddb626dc24352433389f6f54217e9ce41b
MIIGgames <98579374+MIIGgames@users.noreply.github.com>
2023-03-26 01:45:20 -0300

    up

--- js/menu-ancora.js
+++ js/menu-ancora.js
@@ -1,16 +1,16 @@
-const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+const menuLinks = document.querySelectorAll('#menu a[href^="#"]');
 
 function getDistanceFromTheTop(element) {
   const id = element.getAttribute("href");
   return document.querySelector(id).offsetTop;
 }
 
-// function nativeScroll(distanceFromTheTop) {
-//   window.scroll({
-//     top: distanceFromTheTop,
-//     behavior: "smooth",
-//   });
-// }
+function nativeScroll(distanceFromTheTop) {
+    window.scroll({
+        top: distanceFromTheTop,
+        behavior: "smooth",
+    });
+}
 
 function scrollToSection(event) {
   event.preventDefault();
